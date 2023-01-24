8390ecde739f3433eec0566b8735e83877c4d031
Pierrerobeyns <pierre.robeyns@gmail.com>
2023-01-24 02:02:11 +0100

    intro finie

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Intro from "./Components/Intro/Intro";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <Intro />
+    </div>
+  );
+};
+
+export default App;
